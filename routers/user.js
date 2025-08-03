2fd0453df4b78af44b23d4e966dc1b360c9feb18
Chotipong Pancam <emper0rpley@gmail.com>
2025-08-04 01:00:06 +0700

    .

--- routers/user.js
+++ routers/user.js
@@ -7,17 +7,24 @@ const {
     listUserBuyer,
     getUserProfile,
     updateUser,
-    updateimage
+    updateimage,
+    getSellerProfile,
+    updateSeller
 } = require("../controllers/user")
 const upload = require("../Middlewares/upload")
 
-
+//Seller
 router.put("/user/:id", updateStatusSeller)
 router.post("/user/:id", deleteUser)
 router.get("/userSeller", listUserSeller)
+router.get("/profileseller/:id",getSellerProfile)
+router.patch("/profileseller/:id",updateSeller)
+//Buyer
 router.get("/userBuyer", listUserBuyer)
 router.get("/profile/:id", getUserProfile)
 router.patch("/profile/:id",updateUser)
 
+
+//profile image
 router.post("/image/:id",upload.single("image"),updateimage)
 module.exports = router
